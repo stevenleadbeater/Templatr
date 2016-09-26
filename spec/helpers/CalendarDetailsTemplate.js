6d2cd658ede026ec65c0320ed46a90003cd70eec
Steven Leadbeater <stevenleadbeater@live.co.uk>
2016-09-26 22:26:19 +0100

    simplifying data for view binding tests

--- spec/helpers/CalendarDetailsTemplate.js
+++ spec/helpers/CalendarDetailsTemplate.js
@@ -4,7 +4,7 @@ var calendarDetailsTemplate = '<div data-repeater="templatr">';
 calendarDetailsTemplate += '     <div class="ganttChartrRow">';
 calendarDetailsTemplate += '       <span class="ganttChartrRowName">{{name}}</span>';
 calendarDetailsTemplate += '       <span data-repeater="templatr" class="calendarDetails1Row" datasource="{{ days }}">';
-calendarDetailsTemplate += '         <span class="calendarDetails1DayName">';
+calendarDetailsTemplate += '         <span class="calendarDetails1DayName" data-dayname="{{ dayName }}" data-monthname="{{ monthName }}" data-yearname="{{ yearName }}">';
 calendarDetailsTemplate += '           <span data-repeater="templatr" datasource="{{ events }}">';
 calendarDetailsTemplate += '             <span class="{{ className }}" data-id="{{id}}" style="{{width}}" data-number-of-days="{{ numberOfDays }}">{{ name }}</span>';
 calendarDetailsTemplate += '           </span>';
